ca5fe4f82e12c686abd90df42d212050571abc01
hornet67 <samin1105009@gmail.com>
2022-05-26 03:46:14 -1200

    first commit

---
+++ array.py
@@ -0,0 +1,30 @@
+#num = int(input("The number of integer inserted to an array: "))
+#arr = []
+
+#for i in range (0,num):
+ #   val = input()
+  #  arr.append(val)
+
+#print(arr)
+
+arr = [1,2,3,4]
+
+
+i=0
+
+arr = [1,2,3,4,4]
+i=0
+
+while i<len(arr):
+    t = arr[i]
+    if arr[0]<len(arr):
+        if i == t:
+            print('True')
+        
+    i+=1
+
+
+    
+    
+       
+    
